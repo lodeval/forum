adce169e0cd8b4b4979daa91e108fe722fe9e361
Laurent Delluc <laurent_delluc@yahoo.fr>
2020-05-24 22:48:49 +0200

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <AppNav />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ client/src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A fully open source forum made by Coding Garden and Laurent</p>
+      <hr class="my-4">
+      <p>This forum is all about community. Be kind! Be helpful!</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
